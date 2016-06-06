9bcb432c3d89bf249b096a266eb974efd6088479
Konrad Beckmann <konrad.beckmann@gmail.com>
2016-06-07 01:52:45 +0200

    Fork it

--- bmp180/proxy.py
+++ bmp180/proxy.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python2
 
+import os, signal
 import socket
 import sys
 import time
 
-# Add timestamp and proxy data to carbon
-
-
 def handle(d):
-    d = d.strip()
-    if len(d) == 0: return
-    unixtime = str(int(time.time()))
-    dd = d.split('\n')
-    dd = [(x + ' ' + unixtime) for x in dd]
-    d = '\n'.join(dd)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('localhost', 2003))
-    s.sendall(d + '\n')
-    print d
-
-
+	d = d.strip()
+	if len(d) == 0: return
+	unixtime = str(int(time.time()))
+	dd = d.split('\n')
+	dd = [(x + ' ' + unixtime) for x in dd]
+	d = '\n'.join(dd)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(('localhost', 2003))
+	s.sendall(d + '\n')
+	print d
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('0.0.0.0', 1234))
 s.listen(1)
+
+signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+
 while 1:
-    conn, addr = s.accept()
-    print "got client"
-    data = conn.recv(1024)
-    print data
-    handle(data)
-    conn.close()
-    print "done"
+	print "waiting for connection..."
+	conn, addr = s.accept()
+	newpid = os.fork()
+	if newpid == 0:
+		print "fork()"
+		# kill the fork after 1 second
+		signal.alarm(1)
+		data = conn.recv(1024)
+		handle(data)
+		conn.close()
+		print "fork: time to die"
+		os._exit(0)
+		print "fork: wtf?"
 
 print "killed"
 
